479e2cf661a192d65297b0d44da54b58c3f873f2
saaheilKalyani <saaheil0909@gmail.com>
2025-10-06 20:56:02 +0530

    Done

---
+++ 0704. Binary Search E.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int f=0,l=nums.length -1;
+        while (f<=l) {
+            int mid = (f+l)/2; 
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                f = mid + 1; 
+            } else if (nums[mid] > target) {
+                l = mid -1; 
+            }
+        }
+        return -1; 
+    }
+}
