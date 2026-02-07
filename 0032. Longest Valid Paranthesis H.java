628c11d6b8c57ce23a47a5b87564698ef31643d7
Saaheil Kalyani <saaheil0909@gmail.com>
2026-02-07 10:06:35 +0530

    Done

---
+++ 0032. Longest Valid Paranthesis H.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1); 
+        int result = 0;  
+        for (int i=0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push(i); 
+            } else {
+                st.pop(); 
+                if (!st.isEmpty()) {
+                    result = Math.max(result, i-st.peek()); 
+                } else {
+                    st.push(i); 
+                }
+            }
+        }
+        return result; 
+    }
+}
